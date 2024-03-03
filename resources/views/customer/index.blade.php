033e1e48d7c6b84f72bad2de91f19154b026760b
Abdelrahman Ahmed <abdelrahman.elazizy.1999@gmail.com>
2024-03-04 00:34:51 +0200

    products order

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -1,15 +1,57 @@
 <x-app-layout>
-    <x-slot name="header">
+    {{-- <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Dashboard') }}
         </h2>
-    </x-slot>
-
+    </x-slot> --}}
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in as customer!") }}
+        <div class="container mx-auto d-flex gap-3">
+            <div class="order p-6 bg-white rounded-3 shadow w-25">
+                <div class="orders overflow-y-scroll" style="max-height: 265px;">
+                    <div>fsssssssssss</div>
+                    <div>fsssssssssss</div>
+                    <div>fsssssssssss</div>
+                    <div>fsssssssssss</div>
+                    <div>fsssssssssss</div>
+                    <div>fsssssssssss</div>
+                    <div>fsssssssssss</div>
+                    <div>fsssssssssss</div>
+                    <div>fsssssssssss</div>
+                    <div>fsssssssssss</div>
+                    <div>fsssssssssss</div>
+                    <div>fsssssssssss</div>
+                    <div>fsssssssssss</div>
+                    <div>fsssssssssss</div>
+                </div>
+                <div class="noted">
+                    <h3>Notes</h3>
+                    <textarea class="form-control mt-1 mb-2 resize-none" name="" id="" cols="30" rows="10"></textarea>
+                </div>
+                <div class="room d-flex align-items-center gap-3 mb-3">
+                    <h3 class="">Room</h3>
+                    <select class="form-control" name="" id="">
+                        <option value=""></option>
+                    </select>
+                </div>
+                <hr class="">
+                <h1 class="price mt-3 fs-3 mb-5 text-end">EGP 55</h1>
+                <button class="btn btn-light btn-outline-dark float-right">Confirm</button>
+                <div class="clear-both"></div>
+            </div>
+            <div class="products p-6 bg-white rounded-3 shadow">
+                <div class="latest-order py-3">
+                    <h1>Latest Order</h1>
+                </div>
+                <hr>
+                <div class="products-list row py-3">
+                    <div class="prdct col-3  text-light">
+                        <div class="rounded-3 bg-warning position-relative">
+                            <img class="rounded-3" src="{{ asset('images/tea1.png') }}" alt="">
+                            <h1 class="fs-2 text-center py-2">Tea</h1>
+                            <div class="price bg-warning text-center">9 LE</div>
+                            <button class="add bg-warning">Add to order</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,9 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        <link href="{{ asset('css/customerHome.css') }}" rel="stylesheet">
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
