e80763a60996f962b5a1abfa46b239601fdb1c36
Abdelrahman Ahmed <abdelrahman.elazizy.1999@gmail.com>
2024-03-06 03:56:26 +0200

    add modal for delete

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -39,7 +39,25 @@
                                 <form action="{{ route('cart.remove', $item->id) }}" method="POST">
                                     @csrf
                                     @method("DELETE")
-                                    <button class="btn btn-danger px-2 py-0 ">x</button>
+                                    <button type="button" class="btn btn-outline-danger px-2 py-0" data-bs-toggle="modal" data-bs-target="#staticBackdrop{{$item->id}}">x</button>
+                                    <!-- Modal -->
+                                    <div class="modal fade" id="staticBackdrop{{$item->id}}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                                      <div class="modal-dialog modal-dialog-centered">
+                                        <div class="modal-content">
+                                          <div class="modal-header">
+                                            <h5 class="modal-title" id="staticBackdropLabel">Remove !</h5>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                          </div>
+                                          <div class="modal-body">
+                                            Are you sure?
+                                          </div>
+                                          <div class="modal-footer">
+                                            <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">No</button>
+                                            <button type="submit" class="btn btn-outline-danger">Yes</button>
+                                          </div>
+                                        </div>
+                                      </div>
+                                    </div>
                                 </form>
                             </td>
                           </tr>
--- resources/views/customer/myorder.blade.php
+++ resources/views/customer/myorder.blade.php
@@ -39,7 +39,25 @@
                             <form action="{{ route('order.remove', $order->id) }}" method="POST">
                                 @csrf
                                 @method("DELETE")
-                                <button type="submit" class="btn btn-danger btn-outline-danger">Cancel</button>
+                                <button type="button" class="btn btn-danger btn-outline-danger" data-bs-toggle="modal" data-bs-target="#staticBackdrop{{$order->id}}">Cancel</button>
+                                  <!-- Modal -->
+                                  <div class="modal fade" id="staticBackdrop{{$order->id}}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                                    <div class="modal-dialog modal-dialog-centered">
+                                      <div class="modal-content">
+                                        <div class="modal-header">
+                                          <h5 class="modal-title" id="staticBackdropLabel">Cancel !</h5>
+                                          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                          Are you sure?
+                                        </div>
+                                        <div class="modal-footer">
+                                          <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">No</button>
+                                          <button type="submit" class="btn btn-outline-danger">Yes</button>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
                             </form>
                             @endif
                         </td>
